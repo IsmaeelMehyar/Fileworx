9f0da2b8d7bc6bf1b544bcef12e689fcebe1f902
maysq <m2991992>
2018-11-04 13:45:22 +0200

    Add function to check if confirmation message is displayed before task execution

--- src/config/Screen.java
+++ src/config/Screen.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Screen {
-	
-	WebDriver driver = ActionKeywords.driver;
+	static WebDriver driver = ActionKeywords.driver;
+	static WebDriverWait wait;
 	public static void Select_Item () throws Exception {
 		
 		ActionKeywords.driver.findElement(By.cssSelector ("button.btn.btn-primary.dropdown-toggle.btn-start-selection")).click();
@@ -26,10 +28,29 @@ public class Screen {
 		ActionKeywords.driver.findElement(By.cssSelector("button.btn-primary.dropdown-toggle.btn-selected-items")).click();
 	}
 	
-	public static void Select_Task() {
-		ActionKeywords.driver.findElement(By.linkText("Quotation")).click();
+	public static void Select_Task() throws InterruptedException {
+		
+		ActionKeywords.driver.findElement(By.linkText("SLeader Ar")).click();
+		//Open_Tasks();
+		// Check if a confirmation message is displayed, if there is no confirmation message the test will continue and ignore the if statement 
+		if (IsConfirmationMsg()){
+			//If confirmation message is displayed click yes
+			ActionKeywords.driver.findElement(By.id("popup-btn-Yes")).click();
+		}
+		
 	}
 	
+	// This function checks if a confirmation message is displayed when a class is selected from tasks list
+	public static boolean IsConfirmationMsg() throws InterruptedException{
+		try{
+			wait= new WebDriverWait(ActionKeywords.driver, 5);
+			WebElement confirmationMsg= wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("ConfirmPopup")));
+			return confirmationMsg.isDisplayed();
+		}
+		catch (Exception e){
+			return false;
+		}
+	}
 	
 
 }
